5e9b44cf6c68653a4342411fd70dd9b7b219f018
saintajax <saintajax@gmail.com>
2022-10-27 09:31:27 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => {
+      return { [state]: prevState[state] + 1 };
+    });
+  };
+
+  countTotalFeeds() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  calcPositiveFeedsPercent() {
+    if (!this.state.good) {
+      return '0%';
+    } else if (!(this.state.neutral + this.state.bad)) {
+      return '100%';
+    }
+    return `${Math.round((this.state.good * 100) / this.countTotalFeeds())}%`;
+  }
+
+  render() {
+    return (
+      <>
+        <Section text="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section text="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeeds()}
+            positiveFeedback={this.calcPositiveFeedsPercent()}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import { Sec, Title } from './Section.styled';
+import { PropTypes } from 'prop-types';
+
+export const Section = ({ text, children }) => {
+  return (
+    <Sec>
+      <Title>{text}</Title>
+      {children}
+    </Sec>
+  );
+};
+
+Section.propTypes = {
+  text: PropTypes.string.isRequired,
+};
